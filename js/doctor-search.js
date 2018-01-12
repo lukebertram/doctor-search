4c98e159a9eee00df1aa23a30a7cc0950a2f12c5
Luke Bertram <lukebertram@gmail.com>
2018-01-12 13:03:06 -0800

    construct api query url from user-submitted name and symptom

--- js/doctor-search.js
+++ js/doctor-search.js
@@ -3,14 +3,13 @@ import {apiKey} from './../.env';
 export function findDoctor(renderFunction, searchName, searchCondition){
   let promise = new Promise((resolve, reject) => {
     let request = new XMLHttpRequest();
-    const url = "https://api.betterdoctor.com/2016-03-01/doctors?";
     const location = "location=or-portland";//hard-coded for now
     const name = (searchName ? "&name=" + searchName : "");
     const query = (searchCondition ? "&query=" + searchCondition : "");
     const key = "&skip=0&limit=50&user_key=" + apiKey;
+    const url = "https://api.betterdoctor.com/2016-03-01/doctors?" + location + name + query + key;
 
-    console.log(`name: ${name} , condition: ${query}`);
-
+    console.log(`URL: ${url}`);
   });//end of promise
 
   promise.then(function(response) {
